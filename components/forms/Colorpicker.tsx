75a8a765ffda9b10a6bbd3ba2308a730dd7fd37e
TereseBo <terese.boderus@gmail.com>
2023-10-27 15:51:43 +0200

    Added styles for colorpicker

--- components/forms/Colorpicker.tsx
+++ components/forms/Colorpicker.tsx
@@ -1,10 +1,42 @@
+import './colorpicker.scss'
 import { Colorinput } from '@/components/forms/Colorinput'
 import { logger } from '@/functions/logger'
 
 export function ColorPicker({ }) {
     return (
-        <div id="color-picker">
-            <Colorinput id="current-color" label="Warp color" clickhandler={logger} />
-        </div>
+        <form id='colorpick-form'>
+            <div className='form-content-container'>
+                <div>
+                    <p>
+                        Pick a color and click <br />
+                        to add warp/weft/ tie up.
+                    </p>
+                </div>
+                <div>
+                    <div className='color-container'>
+                        <div className="color-box">
+                            <h3>Current <br />color</h3>
+                            <Colorinput id="current-color" label="" clickhandler={logger} />
+                        </div>
+                        <div className="color-box" >
+                            <h3>Previous <br />colors</h3>
+                            <div id="previous-colors">
+                                <div className="previous-color">
+                                    <h5>Warp</h5>
+                                    <Colorinput id="warp" label="Warp" clickhandler={logger} />
+                                    <Colorinput id="weft" label="Weft" clickhandler={logger} />
+                                </div>
+                                <div className="previous-color">
+                                    <h5>Weft</h5>
+                                    <Colorinput id="warp" label="Warp" clickhandler={logger} />
+                                    <Colorinput id="weft" label="Weft" clickhandler={logger} />
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form >
     )
 }
