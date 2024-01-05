c7065c7b06c0c28e48923f2f43d7092674e03757
TereseBo <terese.boderus@gmail.com>
2024-01-05 12:57:46 +0100

    Added calculator functions

--- components/calculator/Warplengthform.tsx
+++ components/calculator/Warplengthform.tsx
@@ -1,19 +1,20 @@
 'use client'
+//TODO: Fix style
+//TODO: Add comments
+//TODO: Cleanup
 import './warplengthform.scss'
 
 import { useState } from 'react'
 
-import { calculateWarpLength } from '@/functions/calculator/warplength'
-import { logger } from '@/functions/logger'
+import { calculateFinishedItemLength,calculateWarpLength } from '@/functions/calculator/warplength'
 import { Item } from '@/types/item'
 import { WarpLengthData } from '@/types/warp'
 
 import { Formsection } from './Formsection'
-import { Itemssection } from './Itemssection'
 export function Warplengthform() {
 
     //TODO: Replace any type 
-    const [warpData, setWarpData]: [WarpLengthData, any]= useState({
+    const [warpData, setWarpData]: [WarpLengthData, any] = useState({
         waste: 50,
         lash_on: 50,
         take_up: 10,
@@ -21,57 +22,98 @@ export function Warplengthform() {
         shrinkage: 15,
         total: 100,
     })
-
+    const [ismatching, setIsmatching] = useState(warpData.total >= (warpData.waste + warpData.lash_on))
+    console.log(ismatching)
     //Recalculates and updates state on change in form
     function bobby(e: React.ChangeEvent<HTMLFormElement>) {
 
         const inputValue = e.target.value
-        const fieldId= e.target.id
-        const stateCopy = { ...warpData}
+        const [fieldId, itemId, itemField] = e.target.id.split(':')
+        const stateCopy = { ...warpData }
 
-        switch (fieldId){
+        switch (fieldId) {
+            case 'piece':
+                stateCopy.items[Number(itemId)][itemField as keyof Item] = inputValue
+                break;
             case 'items':
-                const newItems = new Array(Number(inputValue)).fill({ length: 0, firsthem: 0, secondhem: 0 },)
-                stateCopy['items'] = [...newItems]
+                //creates array with unique item-objects
+                const newItems = new Array(Number(inputValue))
+                for (let i = 0; i < inputValue; newItems[i++] = { length: 50, firsthem: 10, secondhem: 10 });
+                stateCopy.items = newItems
                 break;
             case 'total':
-                stateCopy[fieldId as keyof WarpLengthData]=inputValue
+                stateCopy.total = inputValue
+                const nrOfItems = stateCopy.items.length
+                if (nrOfItems > 0) {
+                    console.log('updated itemlengths')
+                    stateCopy.items = calculateFinishedItemLength(stateCopy)
+                    console.log(stateCopy.items)
+                }
                 break;
             default:
-                stateCopy[fieldId as keyof WarpLengthData]=inputValue
-                stateCopy['total']=calculateWarpLength(stateCopy)
+                stateCopy[fieldId as keyof WarpLengthData] = inputValue
+        }
+        if (fieldId !== 'total') {
+            console.log('updates total')
+            stateCopy['total'] = calculateWarpLength(stateCopy)
+            console.log('updates state')
         }
         setWarpData(stateCopy)
+        console.log(ismatching)
+
     }
 
     return (
-        <form id="warplength-form" name="warplength-form" onChange={ bobby} >
+        <form id="warplength-form" name="warplength-form" onChange={bobby} >
             <h3 className="form-header">Warp Length</h3>
             <Formsection>
                 <label className="sectionlabel" htmlFor="waste">Loom waste: </label>
-                <input className="sectioninput" type="number"  id="waste" name="loom-waste" min="1" max="99" value={(warpData.waste).toString()} onChange={(e) => bobby} /> <p className="unit">cm</p>
+                <input className={warpData.total >= (warpData.waste + warpData.lash_on) ? 'sectioninput' : 'sectioninput notMatching'} type="number" id="waste" name="loom-waste" min="1" max="99" value={Number(warpData.waste).toString()} onChange={(e) => bobby} /> <p className="unit">cm</p>
             </Formsection>
             <Formsection>
                 <label className="sectionlabel" htmlFor="lash_on">Lashing on: </label>
-                <input className="sectioninput" type="number"  id="lash_on" name="lash-on" min="1" max="99" value={(warpData.lash_on).toString()} onChange={(e) => bobby} /> <p className="unit">cm</p>
+                <input className={warpData.total >= (warpData.waste + warpData.lash_on) ? 'sectioninput' : 'sectioninput notMatching'} type="number" id="lash_on" name="lash-on" min="1" max="99" value={Number(warpData.lash_on).toString()} onChange={(e) => bobby} /> <p className="unit">cm</p>
             </Formsection>
             <Formsection>
                 <label className="sectionlabel" htmlFor="take_up">Take-up: </label>
-                <input className="sectioninput" type="number" id="take_up" name="take-up" min="1" max="99" value={(warpData.take_up).toString()} onChange={(e) => bobby} /> <p className="unit"> %</p>
+                <input className="sectioninput" type="number" id="take_up" name="take-up" min="1" max="99" value={Number(warpData.take_up).toString()} onChange={(e) => bobby} /> <p className="unit"> %</p>
             </Formsection>
             <Formsection>
                 <label className="sectionlabel" htmlFor="shrinkage">Shrinkage: </label>
-                <input className="sectioninput" type="number" id="shrinkage" name="shrinkage" min="1" max="50" value={Number(warpData.shrinkage)} onChange={(e) => bobby} /><p className="unit"> %</p>
+                <input className="sectioninput" type="number" id="shrinkage" name="shrinkage" min="1" max="50" value={Number(warpData.shrinkage).toString()} onChange={(e) => bobby} /><p className="unit"> %</p>
             </Formsection>
             <Formsection>
                 <label className="sectionlabel" htmlFor="items">Nr of items: </label>
                 <input className="sectioninput" type="number" id="items" name="items" min="1" max="99"
-                    value={Number(warpData.items.length).toString()} onChange={(e)=>bobby} />
+                    value={Number(warpData.items.length).toString()} onChange={(e) => bobby} />
             </Formsection>
-            <Itemssection nrofitems={warpData.items.length} />
+            <div id="items-container">
+                {warpData.items.map((item, index) =>
+                (
+                    <div key={index} className="item-data" id="item-box">
+                        <h6>{`Piece ${index + 1}`}</h6>
+
+                        <Formsection>
+                            <label className="sectionlabel" htmlFor={`piece_${index}_length`}>Item length:</label>
+                            <input className="sectionlabel" type="number" data-unit="cm" id={`piece:${index}:length`} name={`piece:${index}:length`} min="1" max="10000" value={Number(item['length']).toString()} onChange={(e) => bobby} /> cm
+                        </Formsection>
+                        <Formsection>
+                            <label className="sectionlabel" htmlFor={`piece_${index}_firsthem`}>Fringe / hem:</label>
+                            <input type="number" data-unit="cm" id={`piece:${index}:firsthem`} name={`piece:${index}:firsthem`} min="1" max="99" value={Number(item['firsthem']).toString()} onChange={(e) => bobby} />
+                            cm
+                        </Formsection>
+                        <Formsection>
+                            <label className="sectionlabel" htmlFor={`piece_${index}_secondhem`}>Fringe / hem:</label>
+                            <input type="number" data-unit="cm" id={`piece:${index}:secondhem`} name={`piece:${index}:secondhem`} min="1" max="99" value={Number(item['secondhem']).toString()} onChange={(e) => bobby} />
+                            cm
+                        </Formsection>
+                    </div>
+                )
+                )}
+            </div>
             <Formsection>
                 <label className="sectionlabel" htmlFor="total">Warp length: </label>
-                <input className="sectioninput" type="number" id="total" name="total" min="1" max="10000" value={(warpData.total).toString()} onChange={(e) => bobby} />
+                <input className="sectioninput" type="number" id="total" name="total" min="1" max="10000" value={Number(warpData.total).toString()} onChange={(e) => bobby} />
                 <p className="unit"> cm</p>
             </Formsection>
         </form>
--- functions/calculator/warplength.ts
+++ functions/calculator/warplength.ts
@@ -1,35 +1,65 @@
 'use client'
+//Calculations relating to length
 import { Item } from '@/types/item'
 import { WarpLengthData } from '@/types/warp'
 
 import { roundToTwoDec } from '../utils/roundToTwo'
+
+//Returns required total warplength for warpdata object, assumes finished length is specefied in items
 export function calculateWarpLength(warpLengthData:WarpLengthData):number{
         const {lash_on, waste, shrinkage, take_up, items}= warpLengthData
-        let fixedLength = calculateFixedLength(lash_on, waste);
+        let fixedLength = calculateSumLength(lash_on, waste);
         let itemsLength = undefined
 
-        items.length>0? itemsLength=calculateItemLengths(items, take_up, shrinkage ):itemsLength=0
+        items.length>0? itemsLength=calculateItemWarpLength(items, take_up, shrinkage ):itemsLength=0
         
         const total= warpLength(fixedLength, itemsLength)
-        return roundToTwoDec(total)
+        return Math.floor(total)
 
 }
 
-//Calculations relating to length
-function calculateItemLengths( items:Item[], take_up:number, shrinkage: number):number{
+//Returns required warplength for all items combined, assumes finished length is specefied in items
+function calculateItemWarpLength( items:Item[], take_up:number, shrinkage: number):number{
     const itemLengths:number[]= items.map(item=>Number(item.firsthem)+Number(item.secondhem)+Number(item.length))
     const combinedLength:number=itemLengths.reduce((a,b)=>a+b,0)
-    const afterWeave=combinedLength*(1+take_up/100)
-    const afterWash=afterWeave*(1+shrinkage/100)
-    
-    return roundToTwoDec(afterWash)
+    const beforeWashShrinkage=combinedLength*(1+shrinkage/100)
+    const beforeWeaveIn=beforeWashShrinkage*(1+take_up/100)
+
+    return roundToTwoDec(beforeWeaveIn)
 }
-function calculateFixedLength( ...fixed:number[]){//sums all params
+
+function calculateSumLength( ...fixed:number[]){//sums all params
     let sum = fixed.reduce((a,b)=>Number(a)+Number(b),0)
     return sum
 }
+
 function warpLength(itemsLength: number, fixed:number){
     return itemsLength+fixed
 }
 
+//Returns items with equally distributed (finished) length.
+export function calculateFinishedItemLength( warpLengthData:WarpLengthData): Item[]{
+    let itemLength=0
+    let itemHem=0
+    const {lash_on, waste, shrinkage, take_up, items, total}= warpLengthData
+    console.log('total going in: ', total)
+    const nrOfItems=items.length
+    const combinedLength= total-lash_on-waste
+    if(combinedLength>0){
+  
+    console.log('combined items going in: ', combinedLength)
+    const afterWeaveIn=combinedLength*(1-take_up/100)
+    const afterWashShrinkage= afterWeaveIn*(1-shrinkage/100)
+    console.log('After wash and shrink: ', afterWashShrinkage)
+    const perItemTotal= afterWashShrinkage/nrOfItems
+    console.log('per item tpotal:', perItemTotal)
+    itemLength=Math.ceil(perItemTotal*0.8)
+    itemHem=Math.floor(perItemTotal*0.1)
+    }
+    const calculatedItems: Item[] = new Array(nrOfItems)            
+    for (let i = 0; i < nrOfItems; calculatedItems[i++] = { length:itemLength , firsthem: itemHem, secondhem: itemHem});
+    console.log('calculated items:')
+    console.log(calculatedItems)
 
+    return calculatedItems
+}
