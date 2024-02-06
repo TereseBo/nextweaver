4e4505536319cb4b6e3dacfa2d538b00c3b55930
TereseBo <terese.boderus@gmail.com>
2024-02-06 14:02:08 +0100

    Added submit for preferences

--- components/weaver/Preferences.tsx
+++ components/weaver/Preferences.tsx
@@ -2,12 +2,14 @@
 import './preferences.scss'
 
 import { useContext } from 'react'
+import { useRouter } from 'next/navigation'
 
 import { WeaveContext } from '@/contexts/weavecontext'
+
 export function Preferences(){
 
     const {treadles, setTreadles, shafts, setShafts } = useContext(WeaveContext) as WeaveContextType
-
+    const router = useRouter()
     function updateTreadle(e: React.ChangeEvent<HTMLInputElement>):void{
         setTreadles(+e.target.value)
     }
@@ -15,10 +17,16 @@ export function Preferences(){
     function updateShaft(e: React.ChangeEvent<HTMLInputElement>):void{
         setShafts(+e.target.value)
     }
-    
+function handleSubmit(e:React.FormEvent){
+   
+    console.log(e)
+    e.preventDefault()
+    router.push('/weaver/draft')
+}
+
     return (
-        <div className="form-container">
-            <form id="preferences-form" method="post">
+        <div className="form-container" onSubmit={handleSubmit}>
+            <form id="preferences-form">
                 <h3 className="form-header">Loom information</h3>
                 <div>
                     <label htmlFor="shafts">Number of shafts</label>
@@ -28,7 +36,7 @@ export function Preferences(){
                     <label htmlFor="thredles">Number of thredles</label>
                     <input type="number" name="thredles" id="thredles" placeholder={treadles.toString()} onChange={(e)=>{updateTreadle(e)}}/>
                 </div>
-                <input type="submit" value="Submit"/>
+                <input type='submit' value="Submit" />
             </form>
         </div>
     )
