e0d1c8fe918f3dfd679c83abe32c12790599fde4
TereseBo <terese.boderus@gmail.com>
2024-02-17 12:46:49 +0100

    cleanup

--- components/draft/filehandler/Downloadweave.tsx
+++ components/draft/filehandler/Downloadweave.tsx
@@ -1,7 +1,6 @@
 import { useContext, useEffect, useState } from 'react'
 import Link from 'next/link'
 
-import { TextButton } from '@/components/zSharedComponents/TextButton'
 import { WeaveContext } from '@/contexts/weavecontext'
 
 import { createWeaveObject } from './functions/createWeaveObject'
@@ -17,20 +16,6 @@ export function Downloadweave(){
         }
     }, [warpGrid, treadleGrid, tieUpGrid])
     
-/*     function weaveDownloadHandler(){
-        console.log('click')
-        const weave= createWeaveObject(warpGrid || [], treadleGrid || [], tieUpGrid || [])
-        const weaveJson= JSON.stringify(weave)
-        let downloadLink=document.createElement('a')
-        downloadLink.setAttribute('href','data:text/plain;charset=utf-8,' + encodeURIComponent(json))
-        downloadLink.setAttribute('download', filename)
-        downloadLink.style.display='none'
-        document.body.appendChild(downloadLink)
-        downloadLink.click()
-        document.body.removeChild(downloadLink)
-
-    } */
-     {/* <TextButton text="Download" onClick={weaveDownloadHandler}/> */}
 
     return (
         
