cffa4fd4a56bf15b13635882dcbfe8e9d1e03d7c
TereseBo <terese.boderus@gmail.com>
2024-03-15 00:38:24 +0100

    Restructured options

--- components/draft/DraftContainer.tsx
+++ components/draft/DraftContainer.tsx
@@ -1,13 +1,13 @@
 'use client'
 import { ColorPicker } from '@/components/draft/colorpicker/Colorpicker'
 import { Draft } from '@/components/draft/draft/Draft'
-import { ProjectOptions } from '@/components/draft/ProjectOptions'
-export function DraftContainer(){
-    return(
+import { ProjectOptions } from '@/components/draft/optionbars/ProjectOptions'
+export function DraftContainer() {
+    return (
         <>
-        <ColorPicker />
-        <Draft />
-        <ProjectOptions/>
+            <ColorPicker />
+            <Draft />
+            <ProjectOptions />
         </>
     )
 }
