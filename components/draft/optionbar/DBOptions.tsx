424e467afe38af69fa7a92eda1337215fe075e79
TereseBo <terese.boderus@gmail.com>
2024-03-14 20:58:00 +0100

    renameing

--- components/draft/optionbar/DBOptions.tsx
+++ components/draft/optionbar/DBOptions.tsx
@@ -1,6 +1,6 @@
 import { DbLoadWeave } from '@/components/draft/dbhandler/DbLoadWeave'
 import { DbSaveWeave } from '@/components/draft/dbhandler/DbSaveWeave'
-export function DBOptions() {
+export function DbOptions() {
 
     return (
         <>
