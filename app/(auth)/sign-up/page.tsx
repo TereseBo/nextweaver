1421d5e8c8e38e7e7e3d3709b2c0856ff85dc6ad
TereseBo <terese.boderus@gmail.com>
2024-03-14 23:33:24 +0100

    added clerc

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+//Page for sign-up, only containing content required for sign-up andprovided by clerk
+
+import { SignUp } from '@clerk/nextjs';
+
+export default function Page() {
+  return (
+    <div className='signup_container'>
+      <SignUp />
+    </div>
+  );
+}
