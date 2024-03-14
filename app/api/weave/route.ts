2e60e072d7ce4493ba5c2ff977622dfc2a9e885d
TereseBo <terese.boderus@gmail.com>
2024-03-14 20:32:43 +0100

    Remove file due to build error

--- app/api/weave/route.ts
+++ app/api/weave/route.ts
@@ -1,13 +1,11 @@
-
+/* 
 //Route for actions on single weaves owned by a user
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
 import { getDraft, saveDraft } from '../utils/handleWeave';
 
-export async function GET(
-
-) {
+export async function GET() {
     //Fetches one weave for the user
     const { userId } = auth();
     if (!userId) {
@@ -20,7 +18,7 @@ export async function GET(
         if (!draft) {
             return new NextResponse('You have no saved weaves', { status: 204 });
         }
-        return NextResponse.json({ weaveObject:draft }, { status: 200 });
+        return NextResponse.json({ weaveObject: draft }, { status: 200 });
 
     } catch (error) {
         console.log('api/filehandler', error);
@@ -34,16 +32,14 @@ export async function GET(
 //Inserts a Draft in the draft collection
 export async function POST(
     req: Request,
-
 ) {
-
     const { userId } = auth();
     if (!userId) {
         return new NextResponse('Unauthorized', { status: 401 });
     }
 
     try {
-        const body = await req.json();
+        const body = await req.json()
         const { weaveObject } = body.values
         const draft = await saveDraft(userId, weaveObject)
 
@@ -53,6 +49,26 @@ export async function POST(
         console.log('api/[user]/weaves/[tag]', error);
         return new NextResponse(
             'Ooops, something went wrong when posting the weave',
+            { status: 500 }
+        );
+    }
+} */
+
+//Route for actions on multiple weaves owned by a user
+
+import { NextResponse } from 'next/server';
+
+export async function GET(
+    req: Request,
+
+) {
+    try {
+
+        return NextResponse.json('Reached route api/public/weaves/[tag]', { status: 200 });
+    } catch (error) {
+        console.log('api/(user)/weaves/', error);
+        return new NextResponse(
+            'Ooops, something went wrong when getting the route',
             { status: 500 }
         );
     }
