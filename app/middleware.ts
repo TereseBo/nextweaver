43e2c660dfd30b64f0c47663f7f419c593d2c4f1
TereseBo <terese.boderus@gmail.com>
2024-03-15 09:57:51 +0100

    replaced middleware

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,32 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
-
-export default authMiddleware({
-  // Routes that can be accessed while signed out
-  //publicRoutes: ['/:path*'],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  //ignoredRoutes: ['/no-auth-in-this-route'],
-
-  /*  afterAuth(auth, req, evt) {
-     // Handle users who aren't authenticated
-     if (!auth.userId && !auth.isPublicRoute) {
-     return NextResponse.next();
-      
-     }
-     
-     // If the user is logged in and trying to access a protected route, allow them to access route
-     if (auth.userId && !auth.isPublicRoute) {
-       return NextResponse.next();
-     }
-     // Allow users visiting public routes to access them
-     return NextResponse.next();
-   }, */
-
-});
-
+ 
+export default authMiddleware();
+ 
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
