61147e5d724de06075dd758f0572f71a398ff034
TereseBo <terese.boderus@gmail.com>
2024-03-15 08:05:42 +0100

    Added signin/up pages

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,11 @@
+//Page for sign-in, only containing content required for signin provided by clerk
+
+import { SignIn } from '@clerk/nextjs';
+
+export default function Page() {
+  return (
+    <div className='signin_container'>
+      <SignIn />
+    </div>
+  );
+}
