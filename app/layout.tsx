e6fdd4a1f11274df5f29008aa586c2a274be8d3e
TereseBo <terese.boderus@gmail.com>
2024-03-15 08:12:39 +0100

    Removed ClercProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,23 @@
 import './globals.scss'
 
-import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+
 export const metadata: Metadata = {
   title: 'Weaver',
   description: 'Draft maker',
 }
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   )
 }
