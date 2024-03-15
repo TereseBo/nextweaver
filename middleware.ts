c6c88c13656174a527e1204880444eaac576e2d0
TereseBo <terese.boderus@gmail.com>
2024-03-15 09:32:58 +0100

    updated public routes

--- middleware.ts
+++ middleware.ts
@@ -2,25 +2,25 @@ import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes:  ['/api/:path*', '/weaver/:path*'],
+  publicRoutes: ['/:path*'],
   // Routes that can always be accessed, and have
   // no authentication information
   //ignoredRoutes: ['/no-auth-in-this-route'],
 
-   /*  afterAuth(auth, req, evt) {
-      // Handle users who aren't authenticated
-      if (!auth.userId && !auth.isPublicRoute) {
-      return NextResponse.next();
-       
-      }
+  /*  afterAuth(auth, req, evt) {
+     // Handle users who aren't authenticated
+     if (!auth.userId && !auth.isPublicRoute) {
+     return NextResponse.next();
       
-      // If the user is logged in and trying to access a protected route, allow them to access route
-      if (auth.userId && !auth.isPublicRoute) {
-        return NextResponse.next();
-      }
-      // Allow users visiting public routes to access them
-      return NextResponse.next();
-    }, */
+     }
+     
+     // If the user is logged in and trying to access a protected route, allow them to access route
+     if (auth.userId && !auth.isPublicRoute) {
+       return NextResponse.next();
+     }
+     // Allow users visiting public routes to access them
+     return NextResponse.next();
+   }, */
 
 });
 
